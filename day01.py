9255015a2d961cc386be5a32205c441553d2c79e
HypedAugust <44738421+HypedAugust@users.noreply.github.com>
2024-07-01 22:10:43 +0900

    day02

---
+++ day01.py
@@ -0,0 +1,33 @@
+# 두수의 합
+def solution(num1, num2):
+    answer = num1 + num2
+    return answer
+
+# 두수의 차 
+def solution(num1, num2):
+    answer = num1-num2
+    return answer
+
+# 두수의 곱
+def solution(num1, num2):
+    answer = num1*num2
+    return answer
+
+# 몫 
+def solution(num1, num2):
+    answer = num1//num2
+    return answer
+
+## Day 2 
+# 두 수의 나눗셈. 다시확인 
+def solution(num1, num2):
+    return int(num1/num2*1000)
+
+# 숫자 비교하기 
+def solution(num1, num2):
+    if num1 == num2:
+        return 1
+    elif num1 != num2:
+        return -1
+
+
