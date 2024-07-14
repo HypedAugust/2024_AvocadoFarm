e093a67fb0301683d78df9727754347226bcfa57
HypedAugust <44738421+HypedAugust@users.noreply.github.com>
2024-07-14 09:20:12 +0900

    day 14

--- day13.py
+++ day13.py
@@ -15,3 +15,28 @@ def solution(cipher, code):
 def solution(cipher, code):
     answer = cipher[code-1::code]
     return answer
+                
+# 문자열 my_string이 매개변수로 주어질 때, 
+# 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 
+# return하도록 solution 함수를 완성해주세요.
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i.isupper():
+            answer += i.lower()
+        else:
+            answer += i.upper()
+    return answer
+
+def solution(my_string):
+    return my_string.swapcase()
+
+# 정수 배열 array가 매개변수로 주어질 때, 
+# 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+def solution(array):
+    answer = []
+    biggest = max(array)
+    index = array.index(biggest)
+    answer = [biggest, index]
+    return answer 
